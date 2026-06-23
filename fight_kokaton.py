e08e5ac497e94b1055d4cb02dddb514160fdd000
minato01-ai <hom452063@gmail.com>
2026-06-23 17:46:10 +0900

    不自然な空白をなくしてください#1

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -204,7 +204,6 @@ def main():
                         score.value += 1
         bombs = [bomb for bomb in bombs if bomb is not None]
         beams = [beam for beam in beams if beam is not None]
-        
         beams = [beam for beam in beams if check_bound(beam.rct) == (True, True)]
 
 
